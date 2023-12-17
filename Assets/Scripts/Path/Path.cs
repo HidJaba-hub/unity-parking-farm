d55f6ae0d5fb2f4daba25ed3ad6810dfd4b41e1c
Jacemon <evgeeeha00@mail.ru>
2023-12-17 07:25:39 +0300

    level 4

---
+++ Assets/Scripts/Path/Path.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Entities
+{
+    public class Path : MonoBehaviour
+    {
+        
+        public Color color = Color.green;
+        public float radius = 0.5f;
+        
+        private void OnDrawGizmos()
+        {
+            var isFirst = true;
+            Transform prevNode = null; 
+            
+            foreach (var node in gameObject.GetComponentsInChildren<Transform>())
+            {
+                if (isFirst)
+                {
+                    isFirst = false;
+                    continue;
+                }
+                
+                Gizmos.color = color;
+                
+                Gizmos.DrawSphere(node.position, radius);
+
+                if (prevNode == null)
+                {
+                    prevNode = node;
+                    continue;
+                }
+                Gizmos.DrawLine(prevNode.position, node.position);
+                
+                prevNode = node;
+            }
+        }
+    }
+}
